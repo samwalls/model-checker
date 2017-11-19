7f24399ace5bdc81f070a765eafd19771e429444
samwalls <sam@s-w.io>
2017-11-19 14:21:13 +0000

    add initial project files

---
+++ src/main/java/modelChecker/SimpleModelChecker.java
@@ -0,0 +1,20 @@
+package modelChecker;
+
+import formula.stateFormula.StateFormula;
+import model.Model;
+
+public class SimpleModelChecker implements ModelChecker {
+
+    @Override
+    public boolean check(Model model, StateFormula constraint, StateFormula query) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String[] getTrace() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
